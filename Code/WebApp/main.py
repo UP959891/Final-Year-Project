7c940e6d02d29aae68fcf953aabd63656d626e69
Amr's Desktop <up959891@myport.ac.uk>
2023-04-12 15:32:19 +0100

    First commit

---
+++ Code/WebApp/main.py
@@ -0,0 +1,6 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True) # in production make this false as it means the webserver is re-started every time there is a change in the python code.
