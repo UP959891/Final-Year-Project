47e95a5c94bc02a3495e5f28ffdd61e7ea653c57
Amr's ThinkPad <up959891@myport.ac.uk>
2023-04-13 17:19:17 +0100

    Defined Views and Auth blueprints

---
+++ Code/WebApp/Website/views.py
@@ -0,0 +1,9 @@
+#this file is for storing the routes a user can take navigating the website.
+
+from flask import Blueprint
+
+#Define this file is the BluePrint of this website
+
+views = Blueprint('views', __name__) # dont need to name this the same as file
+
+
