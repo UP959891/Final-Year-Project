0b84efa9dccc23b084e10169ab384dd93e155c2c
Amr's ThinkPad <up959891@myport.ac.uk>
2023-04-14 15:10:00 +0100

    removed authentification code

--- Code/WebApp/Website/views.py
+++ Code/WebApp/Website/views.py
@@ -1,6 +1,6 @@
 #this file is for storing the routes a user can take navigating the website.
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 #Define this file is the BluePrint of this website
 
@@ -8,6 +8,6 @@ views = Blueprint('views', __name__) # dont need to name this the same as file
 
 @views.route('/') #whenever the URL gets to the Home page this will run
 def home():
-    return "<h1>Test<h1/>"
+    return render_template("home.html")
 
 
