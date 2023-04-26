e99d0b7734fa8fcc7fd26344b45b4cb36dc406b2
Amr's ThinkPad <up959891@myport.ac.uk>
2023-04-26 03:15:25 +0100

    Added state machine to chatbot

--- Code/CustomerService_Chatbot/Alt Chatbot/alt_chatbot.py
+++ Code/CustomerService_Chatbot/Alt Chatbot/alt_chatbot.py
@@ -28,67 +28,76 @@ with open('label_encoder.pickle', 'rb') as enc:
 # parameters
 max_len = 20
 
-def getResult(input):
-    inp = input
-    result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),
-                                            truncating='post', maxlen=max_len))
-    return result
+class Chatbot:
 
-def getTag(result):
-     res = result
-     tag = lbl_encoder.inverse_transform([np.argmax(res)])
-     return tag
-
-def cleanupTag(tag):
-    inputTag = tag
-    outputTag = ""
-    for char in inputTag:
-        if char.isalnum():
-            outputTag += char
-    return outputTag
+    def __init__(self):
+        self.state = 'initialise'
 
 
-# def diagnose(input):
-    inp = input
-    result = getResult(inp)
-    tag = getTag(result)
-    for i in data['intents']:
-        if i['tag'] == tag:
-            print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL , np.random.choice(i['responses']))
-            
-        else:
-            print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL , 'I am not sure i understood what the problem is, could you re-state it?')
+    def getResult(self, input):
+        inp = input
+        result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),
+                                                truncating='post', maxlen=max_len))
+        return result
 
-def chat():
-    while True:
-        print(Fore.LIGHTBLUE_EX + "User: " + Style.RESET_ALL, end="")
-        inp = input()
+    def getTag(self, result):
+        res = result
+        tag = lbl_encoder.inverse_transform([np.argmax(res)])
+        return tag
 
-        if inp.lower() == "quit":
-            break
+    def cleanupTag(self, tag):
+        inputTag = tag
+        outputTag = ""
+        for char in inputTag:
+            if char.isalnum():
+                outputTag += char
+        return outputTag
 
+
+    # def diagnose(input):
+        inp = input
         result = getResult(inp)
         tag = getTag(result)
-
-        # cleanup the tag into alphanumeric string.
-        # alNumTagSTR = cleanupTag(tag)
-        # print(alNumTagSTR)
-
-        # if alNumTagSTR[:4] == 'diag':
-        #     print('made it')
-        #     diagnose(inp)
-
-
         for i in data['intents']:
             if i['tag'] == tag:
                 print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL , np.random.choice(i['responses']))
+                
+            else:
+                print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL , 'I am not sure i understood what the problem is, could you re-state it?')
+
+    def chat(self):
+        while True:
+            print(Fore.LIGHTBLUE_EX + "User: " + Style.RESET_ALL, end="")
+            inp = input()
+
+            if inp.lower() == "quit":
                 break
 
+            result = self.getResult(inp)
+            tag = self.getTag(result)
 
-        # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL,random.choice(responses))
+            # cleanup the tag into alphanumeric string.
+            alNumTagSTR = self.cleanupTag(tag)
+            print(alNumTagSTR)
 
+            # if alNumTagSTR[:4] == 'diag':
+            #     print('made it')
+            #     diagnose(inp)
+
+
+            for i in data['intents']:
+                if i['tag'] == tag:
+                    print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL , np.random.choice(i['responses']))
+                    self.state = alNumTagSTR
+                    print(self.state)
+                    break
+
+
+            # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL,random.choice(responses))
+
+chatbot = Chatbot()
 print(Fore.YELLOW + "Start messaging with the bot (type quit to stop)!" + Style.RESET_ALL)
-chat()
+chatbot.chat()
 
 
 
